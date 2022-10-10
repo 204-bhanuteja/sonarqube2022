81e7901be79874292e3126cdf9c62468fab7b191
bhanu <bhanuprakash123321@gmail.com>
2022-10-10 11:50:51 +0530

    Add project files.

---
+++ sonarqube2022/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sonarqube2022.Models;
+using System;
+using System.Diagnostics;
+
+namespace sonarqube2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index()
+        {
+            if (double.IsNaN(num)
+            {
+               return "a is not a number";
+            }
+
+           
+            return "done";
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
